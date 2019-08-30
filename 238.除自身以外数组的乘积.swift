85ea21e3f9dc36e5fdff9dc16893155b53fdccd8
xurunkang <625927650@qq.com>
2019-08-30 20:23:58 +0800

    238.除自身以外数组的乘积
    Change-Id: I36dfdda440686b5f1ca645416fd64dd66b821f3d

---
+++ 238.除自身以外数组的乘积.swift
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=238 lang=swift
+ *
+ * [238] 除自身以外数组的乘积
+ *
+ * https://leetcode-cn.com/problems/product-of-array-except-self/description/
+ *
+ * algorithms
+ * Medium (62.52%)
+ * Likes:    201
+ * Dislikes: 0
+ * Total Accepted:    15.8K
+ * Total Submissions: 25.3K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 给定长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i]
+ * 之外其余各元素的乘积。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * 
+ * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * 
+ * 进阶：
+ * 你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+ * 
+ */
+class Solution {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        // from left to right 
+
+        var i = 1
+        var j = nums.count - 2
+
+        var leftProducts = Array(repeating: 1, count: nums.count)
+        var rightProducts = Array(repeating: 1, count: nums.count)
+
+        while i < nums.count {
+            leftProducts[i] = nums[i-1] * leftProducts[i-1]
+            rightProducts[j] = nums[j+1] * rightProducts[j+1]
+
+            i += 1
+            j -= 1
+        }
+
+        var res: [Int] = []
+
+        for i in 0..<nums.count {
+            res.append(leftProducts[i]*rightProducts[i])
+        }
+
+        return res
+    }
+}
+
