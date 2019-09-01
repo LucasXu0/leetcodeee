5ace4f5c6a175f18edb7d37ddb7f48de0e938727
xurunkang <625927650@qq.com>
2019-09-01 19:00:07 +0800

    155.最小栈

---
+++ 155.最小栈.swift
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=155 lang=swift
+ *
+ * [155] 最小栈
+ *
+ * https://leetcode-cn.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (49.74%)
+ * Likes:    269
+ * Dislikes: 0
+ * Total Accepted:    41.1K
+ * Total Submissions: 82.6K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * 
+ * 
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * 
+ * 
+ * 示例:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * 
+ * 
+ */
+
+class MinStack {
+
+    var minStack: [Int] = []
+    var stack: [Int] = []
+
+    /** initialize your data structure here. */
+    init() {
+        
+    }
+    
+    func push(_ x: Int) {
+        stack.append(x)
+        if minStack.isEmpty || x <= minStack.last! {
+            minStack.append(x)
+        }
+    }
+    
+    func pop() {
+        let last = stack.removeLast()
+        if last == minStack.last! {
+            minStack.removeLast()
+        }
+    }
+    
+    func top() -> Int {
+        return stack.last!
+    }
+    
+    func getMin() -> Int {
+        return minStack.last!
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * let obj = MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * let ret_3: Int = obj.top()
+ * let ret_4: Int = obj.getMin()
+ */
+
