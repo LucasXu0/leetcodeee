903929729f28149ccc5563893bb2ce3e20da8458
xurunkang <625927650@qq.com>
2019-09-01 19:08:31 +0800

    150.逆波兰表达式求值

---
+++ 150.逆波兰表达式求值.swift
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=150 lang=swift
+ *
+ * [150] 逆波兰表达式求值
+ *
+ * https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/description/
+ *
+ * algorithms
+ * Medium (45.80%)
+ * Likes:    65
+ * Dislikes: 0
+ * Total Accepted:    13.8K
+ * Total Submissions: 30.1K
+ * Testcase Example:  '["2","1","+","3","*"]'
+ *
+ * 根据逆波兰表示法，求表达式的值。
+ * 
+ * 有效的运算符包括 +, -, *, / 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+ * 
+ * 说明：
+ * 
+ * 
+ * 整数除法只保留整数部分。
+ * 给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入: ["2", "1", "+", "3", "*"]
+ * 输出: 9
+ * 解释: ((2 + 1) * 3) = 9
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: ["4", "13", "5", "/", "+"]
+ * 输出: 6
+ * 解释: (4 + (13 / 5)) = 6
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+ * 输出: 22
+ * 解释: 
+ * ⁠ ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ * 
+ */
+class Solution {
+    func evalRPN(_ tokens: [String]) -> Int {
+        
+        var stack: [Int] = []
+
+        let op: Set<String> = ["+", "-", "*", "/"]
+
+        for token in tokens {
+            if op.contains(token) {
+                let first = stack.removeLast()
+                let second = stack.removeLast()
+                switch token {
+                case "+": stack.append(second+first)
+                case "-": stack.append(second-first)
+                case "*": stack.append(second*first)
+                case "/": stack.append(second/first)
+                default: break
+                }
+            } else {
+                stack.append(Int(token)!)
+            }
+        }
+
+        return stack.last!
+    }
+}
+
