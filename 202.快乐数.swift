8a7e14d47e8e25083d84efeff31a0cad8c2b8809
xurunkang <625927650@qq.com>
2019-09-04 00:10:59 +0800

    202.快乐数
    Change-Id: I9bdad1da03391f60109a17c654e9da0aba43073c

---
+++ 202.快乐数.swift
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=202 lang=swift
+ *
+ * [202] 快乐数
+ *
+ * https://leetcode-cn.com/problems/happy-number/description/
+ *
+ * algorithms
+ * Easy (54.92%)
+ * Likes:    153
+ * Dislikes: 0
+ * Total Accepted:    26.8K
+ * Total Submissions: 48.9K
+ * Testcase Example:  '19'
+ *
+ * 编写一个算法来判断一个数是不是“快乐数”。
+ * 
+ * 一个“快乐数”定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到
+ * 1。如果可以变为 1，那么这个数就是快乐数。
+ * 
+ * 示例: 
+ * 
+ * 输入: 19
+ * 输出: true
+ * 解释: 
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 0^2 = 1
+ * 
+ * 
+ */
+class Solution {
+    func isHappy(_ n: Int) -> Bool {
+
+        // return _isHappy(n)
+
+        var num = n
+        var sum = 0
+        var visitedSet = Set<Int>()
+
+        while sum != 1 {
+            sum = 0
+            
+            while num != 0 {
+                let remainder = num % 10
+                sum += remainder * remainder
+                num /= 10
+            }
+
+            if visitedSet.contains(sum) {
+                return false
+            }
+
+            num = sum
+            visitedSet.insert(sum)
+        }
+
+        return true
+    }
+
+    private func _isHappy(_ n: Int) -> Bool {
+        var num = n
+        var sum = 0
+
+        // 是不是是所有的数最后都可以化到个位数
+        repeat {
+            sum = 0
+            while num != 0 {
+                let remainder = num % 10
+                sum += remainder * remainder
+                num /= 10
+            }
+            num = sum
+        } while sum > 6
+        
+        return sum == 1
+    }
+}
+
