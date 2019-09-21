22f629b08a5c5eb00851d154ac2f1f8656508ea6
xurunkang <625927650@qq.com>
2019-09-21 13:09:54 +0800

    101.对称二叉树
    Change-Id: Id4af5b1ca03f382afb14e9a5e9c11aa2a40d4556

---
+++ 101.对称二叉树.swift
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=swift
+ *
+ * [101] 对称二叉树
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (48.41%)
+ * Likes:    449
+ * Dislikes: 0
+ * Total Accepted:    56.8K
+ * Total Submissions: 116.9K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ * 
+ * 
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ * 
+ * 
+ * 说明:
+ * 
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        return isMirror(root, root)
+    }
+
+    private func isMirror(_ r1: TreeNode?, _ r2: TreeNode?) -> Bool {
+        if r1 == nil && r2 == nil { return true }
+        guard let r1 = r1, let r2 = r2 else { return false }
+
+        return r1.val == r2.val && isMirror(r1.left, r2.right) && isMirror(r2.left, r1.right)
+    }
+}
+
