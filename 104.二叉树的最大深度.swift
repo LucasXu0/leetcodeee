88490b7d042476775376e02e0e189a9f3c1022bf
xurunkang <625927650@qq.com>
2019-09-21 13:41:33 +0800

    104.二叉树的最大深度
    Change-Id: I11807b36f915f38757847c10b6d8ff0932eda816

---
+++ 104.二叉树的最大深度.swift
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=swift
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (70.89%)
+ * Likes:    351
+ * Dislikes: 0
+ * Total Accepted:    77.6K
+ * Total Submissions: 109.4K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最大深度。
+ * 
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回它的最大深度 3 。
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+
+        func depth(_ node: TreeNode?) -> Int {
+            if let node = node {
+                return max(depth(node.left), depth(node.right)) + 1
+            } else {
+                return 0
+            }
+        }
+
+        return depth(root)
+    }
+}
+
