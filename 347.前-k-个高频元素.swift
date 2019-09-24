6d1c8d960d0ebd0771b58750768273a7d57bfaa1
xurunkang <625927650@qq.com>
2019-09-24 17:11:42 +0800

    347.前-k-个高频元素
    Change-Id: Icfb17d35c7ed47b62385027bea8a3a3a3952ea0f

--- 347.前-k-个高频元素.swift
+++ 347.前-k-个高频元素.swift
@@ -37,6 +37,38 @@
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
         
+        return _heap(nums, k)
+    }
+
+    private func _heap(_ nums: [Int], _ k: Int) -> [Int] {
+        var map: [Int: Int] = [:]
+
+        for num in nums {
+            map[num, default: 0] += 1
+        }
+
+        var heap = Heap<Int>(sort: {
+            return map[$0]! < map[$1]!
+        })
+
+        for v in map {
+            heap.add(v.key)
+            if heap.count > k {
+                heap.poll()
+            }
+        }
+
+        var res: [Int] = []
+        while !heap.isEmpty {
+            res.append(heap.poll()!)
+        }
+
+        res.reverse()
+
+        return res
+    }
+
+    private func _buckets(_ nums: [Int], _ k: Int) -> [Int] {
         // 利用桶排序
 
         var map: [Int: Int] = [:]
@@ -94,4 +126,154 @@ class Solution {
         return res
     }
 }
+
+public struct Heap<T> {
+
+    var nodes = [T]()
+
+    private var orderCriteria: (T, T) -> Bool
+
+    public init(sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+    }
+
+    public init(array: [T], sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+        configureHeap(from: array)
+    }
+
+    private mutating func configureHeap(from array: [T]) {
+        nodes = array
+        for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+            shiftDown(i)
+        }
+    }
+
+    public var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+
+    public var count: Int {
+        return nodes.count
+    }
+
+    @discardableResult public mutating func poll() -> T? {
+        return self.remove()
+    }
+
+    public mutating func add(_ value: T) {
+        self.insert(value)
+    }
+
+    @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+
+    @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+
+    @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+
+    public func peek() -> T? {
+        return nodes.first
+    }
+
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+
+    public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        for value in sequence {
+            insert(value)
+        }
+    }
+
+    public mutating func replace(index i: Int, value: T) {
+        guard i < nodes.count else { return }
+
+        remove(at: i)
+        insert(value)
+    }
+
+    @discardableResult public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+
+    @discardableResult public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+
+        let size = nodes.count - 1
+        if index != size {
+            nodes.swapAt(index, size)
+            shiftDown(from: index, until: size)
+            shiftUp(index)
+        }
+        return nodes.removeLast()
+    }
+
+    internal mutating func shiftUp(_ index: Int) {
+        var childIndex = index
+        let child = nodes[childIndex]
+        var parentIndex = self.parentIndex(ofIndex: childIndex)
+
+        while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(ofIndex: childIndex)
+        }
+
+        nodes[childIndex] = child
+    }
+
+    internal mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+
+    internal mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
+
+}
+
+extension Heap where T: Equatable {
+
+    public func index(of node: T) -> Int? {
+        return nodes.firstIndex(where: { $0 == node })
+    }
+
+    @discardableResult public mutating func remove(node: T) -> T? {
+        if let index = index(of: node) {
+            return remove(at: index)
+        }
+        return nil
+    }
+
+}
+
 
