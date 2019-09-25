ce62963c9fb031cfbe9ba0c2a04cc9a100f8afad
xurunkang <625927650@qq.com>
2019-09-25 16:39:58 +0800

    692.前k个高频单词
    Change-Id: I3e1911f5bd26b12f91ded6f6649ffa63dec8a6dc

---
+++ 692.前k个高频单词.swift
@@ -0,0 +1,249 @@
+/*
+ * @lc app=leetcode.cn id=692 lang=swift
+ *
+ * [692] 前K个高频单词
+ *
+ * https://leetcode-cn.com/problems/top-k-frequent-words/description/
+ *
+ * algorithms
+ * Medium (42.89%)
+ * Likes:    48
+ * Dislikes: 0
+ * Total Accepted:    3.6K
+ * Total Submissions: 8.3K
+ * Testcase Example:  '["i", "love", "leetcode", "i", "love", "coding"]\n2'
+ *
+ * 给一非空的单词列表，返回前 k 个出现次数最多的单词。
+ * 
+ * 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * 输出: ["i", "love"]
+ * 解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+ * ⁠   注意，按字母顺序 "i" 在 "love" 之前。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+ * k = 4
+ * 输出: ["the", "is", "sunny", "day"]
+ * 解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+ * ⁠   出现次数依次为 4, 3, 2 和 1 次。
+ * 
+ * 
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 假定 k 总为有效值， 1 ≤ k ≤ 集合元素数。
+ * 输入的单词均由小写字母组成。
+ * 
+ * 
+ * 
+ * 
+ * 扩展练习：
+ * 
+ * 
+ * 尝试以 O(n log k) 时间复杂度和 O(n) 空间复杂度解决。
+ * 
+ * 
+ */
+class Solution {
+    func topKFrequent(_ words: [String], _ k: Int) -> [String] {
+
+        var freq: [String: Int] = [:]
+
+        for word in words {
+            freq[word, default: 0] += 1
+        }
+
+        var minHeap = Heap<String>(sort: {
+            if freq[$0]! < freq[$1]! {
+                return true
+            } else if freq[$0]! == freq[$1]! && $0 > $1 {
+                return true
+            }
+
+            return false
+        })
+
+        for ele in freq {
+            minHeap.add(ele.key)
+
+            if minHeap.count > k {
+                minHeap.poll()
+            }
+        }
+
+        var res: [String] = []
+
+        while let top = minHeap.poll() {
+            res.append(top)
+        }
+
+        return res.reversed()
+    }
+}
+
+public struct Heap<T> {
+
+    var nodes = [T]()
+
+    private var orderCriteria: (T, T) -> Bool
+
+    public init(sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+    }
+
+    public init(array: [T], sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+        configureHeap(from: array)
+    }
+
+    private mutating func configureHeap(from array: [T]) {
+        nodes = array
+        for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+            shiftDown(i)
+        }
+    }
+
+    public var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+
+    public var count: Int {
+        return nodes.count
+    }
+
+    @discardableResult public mutating func poll() -> T? {
+        return self.remove()
+    }
+
+    public mutating func add(_ value: T) {
+        self.insert(value)
+    }
+
+    public mutating func add<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        self.insert(sequence)
+    }
+
+    @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+
+    @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+
+    @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+
+    public func peek() -> T? {
+        return nodes.first
+    }
+
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+
+    public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        for value in sequence {
+            insert(value)
+        }
+    }
+
+    public mutating func replace(index i: Int, value: T) {
+        guard i < nodes.count else { return }
+
+        remove(at: i)
+        insert(value)
+    }
+
+    @discardableResult public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+
+    @discardableResult public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+
+        let size = nodes.count - 1
+        if index != size {
+            nodes.swapAt(index, size)
+            shiftDown(from: index, until: size)
+            shiftUp(index)
+        }
+        return nodes.removeLast()
+    }
+
+    internal mutating func shiftUp(_ index: Int) {
+        var childIndex = index
+        let child = nodes[childIndex]
+        var parentIndex = self.parentIndex(ofIndex: childIndex)
+
+        while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(ofIndex: childIndex)
+        }
+
+        nodes[childIndex] = child
+    }
+
+    internal mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+
+    internal mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
+
+}
+
+extension Heap where T: Equatable {
+
+    public func index(of node: T) -> Int? {
+        return nodes.firstIndex(where: { $0 == node })
+    }
+
+    @discardableResult public mutating func remove(node: T) -> T? {
+        if let index = index(of: node) {
+            return remove(at: index)
+        }
+        return nil
+    }
+
+}
+
+
