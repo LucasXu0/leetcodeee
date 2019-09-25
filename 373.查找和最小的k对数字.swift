78c36fffcf2aeb04e34cdbd7a4d9d9fa403f9c03
xurunkang <625927650@qq.com>
2019-09-25 13:26:38 +0800

    373.查找和最小的k对数字
    Change-Id: Id4bebf4d1e38d9c0079987831a719b1faf05155d

---
+++ 373.查找和最小的k对数字.swift
@@ -0,0 +1,235 @@
+/*
+ * @lc app=leetcode.cn id=373 lang=swift
+ *
+ * [373] 查找和最小的K对数字
+ *
+ * https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums/description/
+ *
+ * algorithms
+ * Medium (35.78%)
+ * Likes:    38
+ * Dislikes: 0
+ * Total Accepted:    2.4K
+ * Total Submissions: 6.7K
+ * Testcase Example:  '[1,7,11]\n[2,4,6]\n3'
+ *
+ * 给定两个以升序排列的整形数组 nums1 和 nums2, 以及一个整数 k。
+ * 
+ * 定义一对值 (u,v)，其中第一个元素来自 nums1，第二个元素来自 nums2。
+ * 
+ * 找到和最小的 k 对数字 (u1,v1), (u2,v2) ... (uk,vk)。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+ * 输出: [1,2],[1,4],[1,6]
+ * 解释: 返回序列中的前 3 对数：
+ * ⁠    [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+ * 输出: [1,1],[1,1]
+ * 解释: 返回序列中的前 2 对数：
+ * [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: nums1 = [1,2], nums2 = [3], k = 3 
+ * 输出: [1,3],[2,3]
+ * 解释: 也可能序列中所有的数对都被返回:[1,3],[2,3]
+ * 
+ * 
+ */
+class Solution {
+    func kSmallestPairs(_ nums1: [Int], _ nums2: [Int], _ k: Int) -> [[Int]] {
+        
+        return bruteForce(nums1, nums2, k)
+    }
+
+    // 遍历所有数组对的结果，然后排序
+    func bruteForce(_ nums1: [Int], _ nums2: [Int], _ k: Int) -> [[Int]] {
+        let n1 = nums1.count
+        let n2 = nums2.count
+
+        var minHeap = Heap<(Int, Int)>(sort: {
+            $0.0 + $0.1 < $1.0 + $1.1
+        })
+
+        for i in 0..<n1 {
+            for j in 0..<n2 {
+                minHeap.add((nums1[i], nums2[j]))
+            }
+        }
+
+        var res: [[Int]] = []
+
+        // 找出最小的 k 对
+        for _ in 1...k {
+            if let top = minHeap.poll() {
+                res.append([top.0, top.1])
+            } else {
+                break
+            }
+        }
+
+        return res
+    }
+}
+
+public struct Heap<T> {
+
+    var nodes = [T]()
+
+    private var orderCriteria: (T, T) -> Bool
+
+    public init(sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+    }
+
+    public init(array: [T], sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+        configureHeap(from: array)
+    }
+
+    private mutating func configureHeap(from array: [T]) {
+        nodes = array
+        for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+            shiftDown(i)
+        }
+    }
+
+    public var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+
+    public var count: Int {
+        return nodes.count
+    }
+
+    @discardableResult public mutating func poll() -> T? {
+        return self.remove()
+    }
+
+    public mutating func add(_ value: T) {
+        self.insert(value)
+    }
+
+    public mutating func add<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        self.insert(sequence)
+    }
+
+    @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+
+    @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+
+    @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+
+    public func peek() -> T? {
+        return nodes.first
+    }
+
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+
+    public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        for value in sequence {
+            insert(value)
+        }
+    }
+
+    public mutating func replace(index i: Int, value: T) {
+        guard i < nodes.count else { return }
+
+        remove(at: i)
+        insert(value)
+    }
+
+    @discardableResult public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+
+    @discardableResult public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+
+        let size = nodes.count - 1
+        if index != size {
+            nodes.swapAt(index, size)
+            shiftDown(from: index, until: size)
+            shiftUp(index)
+        }
+        return nodes.removeLast()
+    }
+
+    internal mutating func shiftUp(_ index: Int) {
+        var childIndex = index
+        let child = nodes[childIndex]
+        var parentIndex = self.parentIndex(ofIndex: childIndex)
+
+        while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(ofIndex: childIndex)
+        }
+
+        nodes[childIndex] = child
+    }
+
+    internal mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+
+    internal mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
+
+}
+
+extension Heap where T: Equatable {
+
+    public func index(of node: T) -> Int? {
+        return nodes.firstIndex(where: { $0 == node })
+    }
+
+    @discardableResult public mutating func remove(node: T) -> T? {
+        if let index = index(of: node) {
+            return remove(at: index)
+        }
+        return nil
+    }
+
+}
+
+
