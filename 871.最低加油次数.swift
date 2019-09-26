fbc4a94b9d9c007c29d0ecc43bae6c0164043d02
xurunkang <625927650@qq.com>
2019-09-26 13:43:27 +0800

    871.最低加油次数
    Change-Id: Ibe1ed575e06be70ca53947f7ae75430c8a9838d6

---
+++ 871.最低加油次数.swift
@@ -0,0 +1,261 @@
+/*
+ * @lc app=leetcode.cn id=871 lang=swift
+ *
+ * [871] 最低加油次数
+ *
+ * https://leetcode-cn.com/problems/minimum-number-of-refueling-stops/description/
+ *
+ * algorithms
+ * Hard (27.13%)
+ * Likes:    31
+ * Dislikes: 0
+ * Total Accepted:    1.5K
+ * Total Submissions: 5.3K
+ * Testcase Example:  '1\n1\n[]'
+ *
+ * 汽车从起点出发驶向目的地，该目的地位于出发位置东面 target 英里处。
+ *
+ * 沿途有加油站，每个 station[i] 代表一个加油站，它位于出发位置东面 station[i][0] 英里处，并且有 station[i][1]
+ * 升汽油。
+ *
+ * 假设汽车油箱的容量是无限的，其中最初有 startFuel 升燃料。它每行驶 1 英里就会用掉 1 升汽油。
+ *
+ * 当汽车到达加油站时，它可能停下来加油，将所有汽油从加油站转移到汽车中。
+ *
+ * 为了到达目的地，汽车所必要的最低加油次数是多少？如果无法到达目的地，则返回 -1 。
+ *
+ * 注意：如果汽车到达加油站时剩余燃料为 0，它仍然可以在那里加油。如果汽车到达目的地时剩余燃料为 0，仍然认为它已经到达目的地。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：target = 1, startFuel = 1, stations = []
+ * 输出：0
+ * 解释：我们可以在不加油的情况下到达目的地。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：target = 100, startFuel = 1, stations = [[10,100]]
+ * 输出：-1
+ * 解释：我们无法抵达目的地，甚至无法到达第一个加油站。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：target = 100, startFuel = 10, stations =
+ * [[10,60],[20,30],[30,30],[60,40]]
+ * 输出：2
+ * 解释：
+ * 我们出发时有 10 升燃料。
+ * 我们开车来到距起点 10 英里处的加油站，消耗 10 升燃料。将汽油从 0 升加到 60 升。
+ * 然后，我们从 10 英里处的加油站开到 60 英里处的加油站（消耗 50 升燃料），
+ * 并将汽油从 10 升加到 50 升。然后我们开车抵达目的地。
+ * 我们沿途在1两个加油站停靠，所以返回 2 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= target, startFuel, stations[i][1] <= 10^9
+ * 0 <= stations.length <= 500
+ * 0 < stations[0][0] < stations[1][0] < ... < stations[stations.length-1][0] <
+ * target
+ *
+ *
+ */
+class Solution {
+    func minRefuelStops(_ target: Int, _ startFuel: Int, _ stations: [[Int]]) -> Int {
+
+        var maxHeap = Heap<Int>(sort: >)
+
+        var res = 0
+
+        var stations = stations
+        stations.append([target, 0])
+
+        var remainFuel = startFuel
+
+        var prev = 0
+
+        for station in stations {
+            let loc = station[0]
+            let fuel = station[1]
+
+            remainFuel -= loc - prev
+
+            while remainFuel < 0, let maxFuel = maxHeap.poll() {
+                remainFuel += maxFuel
+                res += 1
+            }
+
+            if remainFuel < 0 {
+                return -1
+            }
+
+            prev = loc
+            maxHeap.add(fuel)
+        }
+
+        return res
+    }
+}
+
+public struct Heap<T> {
+
+    var nodes = [T]()
+
+    private var orderCriteria: (T, T) -> Bool
+
+    public init(sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+    }
+
+    public init(array: [T], sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+        configureHeap(from: array)
+    }
+
+    private mutating func configureHeap(from array: [T]) {
+        nodes = array
+        for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+            shiftDown(i)
+        }
+    }
+
+    public var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+
+    public var count: Int {
+        return nodes.count
+    }
+
+    @discardableResult public mutating func poll() -> T? {
+        return self.remove()
+    }
+
+    public mutating func add(_ value: T) {
+        self.insert(value)
+    }
+
+    public mutating func add<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        self.insert(sequence)
+    }
+
+    @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+
+    @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+
+    @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+
+    public func peek() -> T? {
+        return nodes.first
+    }
+
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+
+    public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        for value in sequence {
+            insert(value)
+        }
+    }
+
+    public mutating func replace(index i: Int, value: T) {
+        guard i < nodes.count else { return }
+
+        remove(at: i)
+        insert(value)
+    }
+
+    @discardableResult public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+
+    @discardableResult public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+
+        let size = nodes.count - 1
+        if index != size {
+            nodes.swapAt(index, size)
+            shiftDown(from: index, until: size)
+            shiftUp(index)
+        }
+        return nodes.removeLast()
+    }
+
+    internal mutating func shiftUp(_ index: Int) {
+        var childIndex = index
+        let child = nodes[childIndex]
+        var parentIndex = self.parentIndex(ofIndex: childIndex)
+
+        while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(ofIndex: childIndex)
+        }
+
+        nodes[childIndex] = child
+    }
+
+    internal mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+
+    internal mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
+
+}
+
+extension Heap where T: Equatable {
+
+    public func index(of node: T) -> Int? {
+        return nodes.firstIndex { (f) -> Bool in
+            return f == node
+        }
+    }
+
+    @discardableResult public mutating func remove(node: T) -> T? {
+        if let index = index(of: node) {
+            return remove(at: index)
+        }
+        return nil
+    }
+
+}
+
