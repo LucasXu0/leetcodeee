61b2a36d961141392e0ba09faf21953d12b969f2
xurunkang <625927650@qq.com>
2019-10-08 19:26:58 +0800

    114.二叉树展开为链表
    Change-Id: Ib443eb60c0baaabbb24d82ea94f528cb12a15807

---
+++ 114.二叉树展开为链表.swift
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=swift
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (65.41%)
+ * Likes:    187
+ * Dislikes: 0
+ * Total Accepted:    15.1K
+ * Total Submissions: 23.1K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为链表。
+ * 
+ * 例如，给定二叉树
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 将其展开为：
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    
+    private var pre: TreeNode?
+
+    func flatten(_ root: TreeNode?) {
+
+        guard let root = root else { return }
+
+        flatten(root.right)
+        flatten(root.left)
+
+        root.right = pre
+        root.left = nil
+
+        pre = root
+    }
+}
+// @lc code=end
+
